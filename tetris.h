a386799bb26c07bca57850e2c0f3f44c08763891
schihwa <schihwa109@gmail.com>
2023-04-14 08:35:44 +0100

    added header file

--- tetris.h
+++ tetris.h
@@ -1,5 +1,6 @@
-#ifndef mainMenu.h
-#define mainMenu.h
-void mainMenuGui(ALLEGRO_DISPLAY *display, ALLEGRO_FONT *font, ALLEGRO_EVENT_QUEUE *event_queue, ALLEGRO_EVENT event, int width, int height);
+#pragma once 
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
 
-#endif
+void mainMenuGui(ALLEGRO_DISPLAY *display, ALLEGRO_FONT *font, ALLEGRO_EVENT_QUEUE *event_queue, ALLEGRO_EVENT event, int width, int height);
